13bf11ea643d726c075cf1f5bafbc96733f9288c
Dara <Dara@128.4.109.145>
2017-02-18 14:30:02 -0500

    Values committed.

--- Lab1213/src/Lab1.java
+++ Lab1213/src/Lab1.java
@@ -4,18 +4,53 @@ public class Lab1 {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.println("Years to Work:");
-		int YearstoWork = input.nextInt();
-		System.out.println("Annual Return:");
-		int AnnualReturn = input.nextInt();
-		System.out.println("Years Retired:");
-		int YearsRetired = input.nextInt();
-		System.out.println("Annual Return Two:");
-		int AnnualReturnTwo = input.nextInt();
-		System.out.println("Required Income:");
-		double RequiredIncome = input.nextDouble();
-		System.out.println("Monthly SSI:");
-		double MonthlySSI = input.nextDouble();
+		
+		System.out.print("Years to Work: ");
+			int YearstoWork = input.nextInt();
+			System.out.print("\n");
+			
+		System.out.print("Annual Return: ");
+			double AnnualReturn = input.nextDouble();
+			System.out.print("\n");
+			
+			while (AnnualReturn > 20 | AnnualReturn < 0) 
+			{
+				System.out.print("Invalid Input, enter new value: ");
+				AnnualReturn = input.nextDouble();
+				System.out.print("\n");
+			}
+			
+			
+			
+		
+		System.out.print("Years Retired: ");
+			int YearsRetired = input.nextInt();
+			System.out.print("\n");
+		
+		System.out.print("Annual Return Two: ");
+			double AnnualReturnTwo = input.nextDouble();
+			System.out.print("\n");
+			
+			while (AnnualReturnTwo > 20 | AnnualReturnTwo < 0) 
+			{
+				System.out.print("Invalid Input, enter new value: ");
+				AnnualReturnTwo = input.nextDouble();
+				System.out.print("\n");
+			}
+		
+		System.out.print("Required Income: ");
+			double RequiredIncome = input.nextDouble();
+			System.out.print("\n");
+		
+		System.out.print("Monthly SSI: ");
+			double MonthlySSI = input.nextDouble();
+			System.out.print("\n");
+		
+			
+		input.close();
 	}
-
 }
+
+
+
+	
